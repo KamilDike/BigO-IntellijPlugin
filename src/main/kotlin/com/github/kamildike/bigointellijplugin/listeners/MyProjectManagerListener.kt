05f560ca5aa4b06403cb6ad745398ac9f1f8c684
GitHub Action <action@github.com>
2021-03-12 11:59:09 +0000

    Template cleanup

--- src/main/kotlin/com/github/kamildike/bigointellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/kamildike/bigointellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.kamildike.bigointellijplugin.listeners
 
+import com.github.kamildike.bigointellijplugin.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/kamildike/bigointellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.kamildike.bigointellijplugin.services
+
+import com.github.kamildike.bigointellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
