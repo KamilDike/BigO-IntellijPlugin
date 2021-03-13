092af073c6ecc3bed4b6245e5ef123388133b7eb
maciejlewandowski5 <64683954+maciejlewandowski5@users.noreply.github.com>
2021-03-13 10:44:02 +0100

    Comparing classes was added

--- src/main/java/com/github/kamildike/bigointellijplugin/BigO.java
+++ src/main/java/com/github/kamildike/bigointellijplugin/BigO.java
@@ -1,5 +1,6 @@
 package com.github.kamildike.bigointellijplugin;
 
+import com.github.kamildike.bigointellijplugin.model.ClassComparator;
 import com.github.kamildike.bigointellijplugin.model.Notation;
 import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.annotation.AnnotationHolder;
@@ -14,27 +15,29 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiManager;
+import com.intellij.psi.PsiRecursiveElementWalkingVisitor;
 import com.intellij.util.ui.UI;
 import org.apache.batik.dom.events.NodeEventTarget;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
 
-class BigOn extends AnAction  {
+class BigOn extends AnAction {
 
 
     @Override
     public void update(AnActionEvent e) {
 
-        final Project project = e.getProject();
-        final Editor editor = e.getData(CommonDataKeys.EDITOR);
+        PsiElement method = e.getData(LangDataKeys.PSI_ELEMENT);
+        PsiElement parent = e.getData(LangDataKeys.PSI_ELEMENT).getContext();
 
-        boolean menuAllowed = false;
-        if(project != null
-                && editor != null){
-            menuAllowed = !editor.getCaretModel().getAllCarets().isEmpty();
-        };
+        if (parent != null) {
+            e.getPresentation().setEnabledAndVisible(
+                    ClassComparator.getInstance().isRecognizedClass(parent));
+        }
+
     }
 
 
@@ -55,25 +58,25 @@ class BigOn extends AnAction  {
         PsiElement method = e.getData(LangDataKeys.PSI_ELEMENT);
         PsiElement parent = e.getData(LangDataKeys.PSI_ELEMENT).getContext();
 
-       // if (PsiManager.getInstance(project).areElementsEquivalent(parent,))
 
-        if(method.getText().equals("public int indexOf(java.lang.Object o) { /* compiled code */ }")){
-            if(parent.getText().contains("public class ArrayList <E> extends java.util.AbstractList<E> implements java.util.List<E>, java.util.RandomAccess, java.lang.Cloneable, java.io.Serializable {\n" +
+        if (method.getText().equals("public int indexOf(java.lang.Object o) { /* compiled code */ }")) {
+            if (parent.getText().contains("public class ArrayList <E> extends java.util.AbstractList<E> implements java.util.List<E>, java.util.RandomAccess, java.lang.Cloneable, java.io.Serializable {\n" +
                     "    private static final long serialVersionUID = 8683452581122892189L;\n" +
                     "    private static final int DEFAULT_CAPACITY = 10;\n"
-            )){
+            )) {
                 notation = Notation.ON;
             }
 
         }
 
 
-        if(notation!=null) {
-            JBPopupFactory.getInstance().createMessage(Notation.getLabel(notation)).showInBestPositionFor(editor);
+        if (notation != null) {
+            JBPopupFactory.getInstance().
+                    createMessage(Notation.getLabel(notation)).showInBestPositionFor(editor);
 
-        Notation finalNotation = notation;
-        Runnable runnable = () -> document.insertString(editor.getCaretModel().getPrimaryCaret().getVisualLineStart()
-                    , "// has time complexity: " + Notation.getLabel(finalNotation)+"\n");
+            Notation finalNotation = notation;
+            Runnable runnable = () -> document.insertString(editor.getCaretModel().getPrimaryCaret().getVisualLineStart()
+                    , "// has time complexity: " + Notation.getLabel(finalNotation) + "\n");
             WriteCommandAction.runWriteCommandAction(project, runnable);
         }
     }
